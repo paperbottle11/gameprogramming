4d83e44c1492ec052bd025769145ba196c4f0a8c
paperbottle11 <xjp2006x@gmail.com>
2024-01-18 13:37:30 -0600

    Please work, pages

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
         super().end_headers()
+        
 
 Handler = MyHTTPRequestHandler
 
